330f5edbbc2b44ba1b6bbdbc84cfd103984f9830
Maxim <kolmax94@gmail.com>
2011-05-29 18:55:50 +0400

    Implement master server worker

--- worker.py
+++ worker.py
@@ -91,3 +91,69 @@ class LocalIPCWorker(AbstractWorker):
                 c += 1
             else:
                 self.ready_blocks.append(r)
+
+import socket, select, threading
+import interface
+
+class NetworkWorker(AbstractWorker):
+    def __init__(self, bounds, screen_bounds, quality, address="127.0.0.1", port=7200, shift=0):
+        AbstractWorker.__init__(self, bounds, screen_bounds, quality, shift)
+
+        self.master_socket = socket.socket()
+        self.master_socket.settimeout(5.0)
+        self.master_socket.bind((socket.gethostbyname(address), port))
+        self.master_socket.listen(1)
+        
+        self.sockets = {}
+        self.ready = False
+
+    def _worker_thread(self):
+        gen = self.blocks()
+        while 1:
+            sockets, w, x = select.select(self.sockets.keys(), [], [], 5)
+            for s in sockets:
+                try:
+                    msg = interface.Message.load(s)
+                except ValueError:
+                    if self.sockets[s]['state'] != 'uninitialized':
+                        print "Node {0}({1}) disconnected".format(self.sockets[s]['hostname'], self.sockets[s]['address'])
+                    s.close()
+                    del self.sockets[s]
+                    continue
+                if msg.type == interface.MSGT_ANNOUNCE:
+                    self.sockets[s]['hostname'] = msg.hostname
+                    self.sockets[s]['state'] = 'waiting'
+                    interface.Message(interface.MSGT_ANNOUNCE_RESP, clients=len(self.sockets)).send(s)
+                    print "Got connection from {0}({1})".format(self.sockets[s]['hostname'], self.sockets[s]['address'])
+                if msg.type == interface.MSGT_RESPONSE:
+                    self.sockets[s]['state'] = 'waiting'
+                    self.ready_blocks.append((msg.block, msg.set))
+
+            for s in self.sockets:
+                if not self.sockets[s]['state'] == 'waiting':
+                    continue
+                try:
+                    block = gen.next()
+                except StopIteration:
+                    self.ready = True
+                    return
+                interface.Message(interface.MSGT_REQUEST,
+                                block=block,
+                                scale=(self.xstep, self.ystep),
+                                quality=self.quality).send(s)
+                self.sockets[s]['state'] = 'running'
+
+
+    def run(self):
+        threading.Thread(target=self._worker_thread).start()
+        while not self.ready:
+            try:
+                client, addr = self.master_socket.accept()
+            except socket.timeout:
+                continue
+            self.sockets[client] = {'address': addr[0],
+                                    'state': 'uninitialized'}
+        for s in self.sockets:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+        self.master_socket.close()
