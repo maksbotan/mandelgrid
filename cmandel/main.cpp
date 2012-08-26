fc5896f8340aa00315d9f7e141642d0ee2d5b312
Maxim Koltsov <maksbotan@gentoo.org>
2012-08-26 20:27:56 +0400

    Add cmake build system

--- cmandel/main.cpp
+++ cmandel/main.cpp
@@ -1,9 +1,15 @@
 
+#include "config.h"
+
+#ifdef HAVE_CLOCK
 #include <time.h>
+#endif
 #include <iostream>
 
 #include "mandel.h"
+#ifdef HAVE_GD
 #include "gdinterface.h"
+#endif
 
 #define WIDTH 1200
 #define HEIGHT 800
@@ -22,17 +28,27 @@ int main(void){
         data
     );
 
+#ifdef HAVE_CLOCK
     clock_t time_start = clock();
+#endif
     renderer.render();
+#ifdef HAVE_CLOCK
     clock_t time_stop = clock();
-
     std::cout << "Rendered in " << (double)(time_stop - time_start) / CLOCKS_PER_SEC << " seconds" << std::endl;
+#endif
 
+#ifdef HAVE_GD
+#ifdef HAVE_CLOCK
     time_start = clock();
+#endif
     save_png(data, WIDTH, HEIGHT, QUALITY, "mandelbrot.png");
+#ifdef HAVE_CLOCK
     time_stop = clock();
-
     std::cout << "Made PNG in " << (double)(time_stop - time_start) / CLOCKS_PER_SEC << " seconds" << std::endl;
+#endif
+#else
+    std::cout << "This is quite useless without LibGD support :)" << std::endl;
+#endif
 
     delete []data;
 
